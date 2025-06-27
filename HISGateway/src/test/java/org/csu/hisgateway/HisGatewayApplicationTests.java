6e2ac0e7650b6127a29dc18ec2f01085a40edb25
ONeofSu <148432545+ONeofSu@users.noreply.github.com>
2025-06-27 11:23:59 +0800

    Gateway config
    初步配置Gateway网关

---
+++ HISGateway/src/test/java/org/csu/hisgateway/HisGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.hisgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
