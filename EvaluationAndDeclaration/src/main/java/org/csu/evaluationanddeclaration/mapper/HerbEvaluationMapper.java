aadabf5582436cca686e10f0ae560f4fc6f1a063
ClemT <1442596222@qq.com>
2025-06-29 19:56:11 +0800

    基本实现评价录入

---
+++ EvaluationAndDeclaration/src/main/java/org/csu/evaluationanddeclaration/mapper/HerbEvaluationMapper.java
@@ -0,0 +1,17 @@
+package org.csu.evaluationanddeclaration.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+import org.csu.evaluationanddeclaration.entity.HerbEvaluation;
+
+import java.util.List;
+
+
+public interface HerbEvaluationMapper extends BaseMapper<HerbEvaluation> {
+    @Select("SELECT * FROM herb_evaluation WHERE evaluation_id = #{evaluationId}")
+    HerbEvaluation getEvaluation(int evaluationId);
+    @Select("SELECT COUNT(*) FROM herb_evaluation")
+    Long count();
+    @Select("SELECT * FROM herb_evaluation WHERE herb_id = #{herbId}")
+    List<HerbEvaluation> getEvaluationsByHerbId(int herbId);
+}
---
+++ EvaluationAndDeclaration/src/main/java/org/csu/evaluationanddeclaration/mapper/HerbRatingMapper.java
@@ -0,0 +1,16 @@
+package org.csu.evaluationanddeclaration.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.csu.evaluationanddeclaration.entity.HerbRating;
+import org.springframework.stereotype.Repository;
+
+
+public interface HerbRatingMapper extends BaseMapper<HerbRating> {
+    @Select("SELECT COUNT(*) FROM herb_rating")
+    Long count();
+
+    @Select("SELECT * FROM herb_rating WHERE herb_id = #{herbId}")
+    HerbRating selectByHerbId(int herbId);
+}
