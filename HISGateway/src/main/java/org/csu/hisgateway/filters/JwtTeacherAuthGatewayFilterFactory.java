dda75f352b242bcb094557a5452905fabf05f644
ONeofSu <148432545+ONeofSu@users.noreply.github.com>
2025-06-30 17:51:03 +0800

    Teacher鉴权实现

---
+++ HISGateway/src/main/java/org/csu/hisgateway/filters/JwtTeacherAuthGatewayFilterFactory.java
@@ -0,0 +1,66 @@
+package org.csu.hisgateway.filters;
+
+import org.csu.hisgateway.utils.JwtUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.util.StringUtils;
+
+public class JwtTeacherAuthGatewayFilterFactory extends AbstractGatewayFilterFactory<JwtTeacherAuthGatewayFilterFactory.Config> {
+    private final JwtUtil jwtUtil;
+
+    JwtTeacherAuthGatewayFilterFactory(JwtUtil jwtUtil) {
+        super(JwtTeacherAuthGatewayFilterFactory.Config.class);
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public GatewayFilter apply(JwtTeacherAuthGatewayFilterFactory.Config config) {
+        return (exchange, chain) -> {
+            //请求头获取Token
+            String token = exchange.getRequest()
+                    .getHeaders()
+                    .getFirst(HttpHeaders.AUTHORIZATION);
+
+            //格式是否合法
+            if (!StringUtils.hasText(token) || !token.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            //提取Token
+            token = token.substring(7);
+
+            //验证有效
+            if (!jwtUtil.validateToken(token)) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            ServerHttpRequest request = exchange.getRequest().mutate()
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)  // 保留原始 Token
+                    .build();
+
+            //提取权限并验证
+            Integer userRoleLevel = jwtUtil.extractRoleLevel(token);
+            if (userRoleLevel == null) {
+                exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+                return exchange.getResponse().setComplete();
+            }
+
+            if(userRoleLevel!=2){
+                //System.out.println("userRoleLevel:"+userRoleLevel);
+                exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+                return exchange.getResponse().setComplete();
+            }
+
+            //继续路由
+            return chain.filter(exchange.mutate().request(request).build());
+        };
+    }
+
+    public static class Config {
+    }
+}
