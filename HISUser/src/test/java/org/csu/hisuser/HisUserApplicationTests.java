561bc8816a05b0e988f95e1bc77b4b4fa4e42e89
ONeofSu <148432545+ONeofSu@users.noreply.github.com>
2025-11-18 15:24:49 +0800

    Update HisUserApplicationTests.java

--- HISUser/src/test/java/org/csu/hisuser/HisUserApplicationTests.java
+++ HISUser/src/test/java/org/csu/hisuser/HisUserApplicationTests.java
@@ -16,13 +16,8 @@ class HisUserApplicationTests {
 
     @Test
     void contextLoads() {
-        User user = new User();
-        user.setUsername("admin");
-        user.setPassword("123456");
-        user.setPhone("1111111");
-        user.setEmail("admin@gmail.com");
-        System.out.println(authService.register(user));
-        System.out.println(authService.login("admin", "123456"));
-        System.out.println(authService.getUserIdFromToken("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsInVzZXJDYXRlZ29yeSI6MSwiaWF0IjoxNzUxMjAxODk1LCJleHAiOjE3NTE0NjEwOTV9.npZOYIQj5Guhmqe4ECzJ5tEVlF1sz4qGKCC2vS50HvE"));
+        System.out.println(userService.getUserById(2));
+        System.out.println(userService.getUserById(4));
+        System.out.println(userService.getUserById(6));
     }
 }
